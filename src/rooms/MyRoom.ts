1ed42be50d7c826dcdde3f390d688acf28fb5e02
ankman115 <ankmanone@gmail.com>
2025-07-18 13:24:13 +0300

    Update MyRoom.ts

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -16,55 +16,57 @@ export class MyRoom extends Room<MyRoomState> {
             player.y = data['y'];
             player.z = data["z"];
 
-          
+
             player.nickname = data["nickname"];
-          
+
             player.fbutton = data["fbutton"];
-          
+
             player.xchar = data["xchar"];
             player.ychar = data['ychar'];
             player.zchar = data["zchar"];
-          
+
             player.xcharmain = data["xcharmain"];
             player.ycharmain = data['ycharmain'];
             player.zcharmain = data["zcharmain"];
-          
+
             player.xcharteker1 = data["xcharteker1"];
             player.ycharteker1 = data['ycharteker1'];
             player.zcharteker1 = data["zcharteker1"];
-          
+
             player.xcharteker2 = data["xcharteker2"];
             player.ycharteker2 = data['ycharteker2'];
             player.zcharteker2 = data["zcharteker2"];
-          
+
             player.xcharteker3 = data["xcharteker3"];
             player.ycharteker3 = data['ycharteker3'];
             player.zcharteker3 = data["zcharteker3"];
-          
+
             player.xcharteker4 = data["xcharteker4"];
             player.ycharteker4 = data['ycharteker4'];
             player.zcharteker4 = data["zcharteker4"];
-          
+
             player.playerx = data["playerx"];
             player.playery = data['playery'];
             player.playerz = data["playerz"];
-          
+
             player.playerxrot = data["playerxrot"];
             player.playeryrot = data['playeryrot'];
             player.playerzrot = data["playerzrot"];
-          
+
             player.playerxpos = data["playerxpos"];
             player.playerzpos = data["playerzpos"];
-          
-          player.numcar = data["numcar"];
+
+            player.numcar = data["numcar"];
             player.colorcar = data["colorcar"];
+            player.myid = data["myid"];
+            player.redorgreen = data["redorgreen"];
         });
     }
 
     onJoin(client: Client, options: any) {
         // Randomize player position on initializing.
         const newPlayer = new Player();
-        
+
         this.state.players.set(client.sessionId, newPlayer);
         console.log(client.sessionId, "joined!");
     }
